cbb453428a6059e5655efcfd16e1c6f17c7c43ff
Mark <mbeatt@hotmail.com>
2022-04-11 17:40:49 -0400

    installed @angular/fire and created material modul

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ohrc-a4b31',
+    appId: '1:1054056354538:web:0d58ee327148edf4be7d93',
+    storageBucket: 'ohrc-a4b31.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBl_3f2y0E49mMPS1QRhBsogIcCzRZ1zzg',
+    authDomain: 'ohrc-a4b31.firebaseapp.com',
+    messagingSenderId: '1054056354538',
+  },
   production: false
 };
 
